d2fed86761a06d6299aa84b4383a31cd75919d77
AntonioPrime <AntonioPrime@ya.ru>
2017-01-24 23:47:19 +0300

    fake login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,26 @@ import * as $ from 'jquery';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   ngOnInit(): void {
+    //todo need to implement on angular
     //toggle menu function
-    $("#menu-toggle").click(function(e) {
+    $("#menu-toggle").click(function (e) {
       e.preventDefault();
       $("#wrapper").toggleClass("toggled");
+      //test hide login form
+      $("login").toggle();
+      //test hide fake login
+      $("#menu-toggle").toggle();
     });
+  }
+
+  public logout(): void {
+    //mock logout
+    $("#wrapper").toggleClass("toggled");
+    //test show login form
+    $("login").toggle();
+    //test show fake login
+    $("#menu-toggle").toggle();
   }
 }
