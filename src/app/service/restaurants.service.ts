9dc4241a97fef1591e7a89a296a9661ccc99998a
AntonioPrime <AntonioPrime@ya.ru>
2017-01-24 17:51:46 +0300

    first environment implementation

--- src/app/service/restaurants.service.ts
+++ src/app/service/restaurants.service.ts
@@ -2,10 +2,11 @@ import {Injectable} from "@angular/core";
 import {Restaurant} from "../model/restaurant";
 import {Observable} from "rxjs";
 import {Http} from "@angular/http";
+import {Url} from "../url";
 
 @Injectable()
 export class RestaurantsService {
-  private restaurantsUrl = 'http://localhost:8080/restaurants';
+  private restaurantsUrl = Url.getUrl('/restaurants');
 
   constructor(private http: Http) {}
 
